da8e87662660e922b5be2779bc9330d6f76947cc
Допи <57643140+XDopi@users.noreply.github.com>
2020-06-30 20:29:54 +0300

    Update grayscale.js

--- commands/image/grayscale.js
+++ commands/image/grayscale.js
@@ -1,20 +1,21 @@
 const { createCanvas, loadImage } = require('canvas');
 const { grayscale } = require('../../util/canvas');
-
 exports.run = async (bot, message, args) => {
-if(!message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES')) return message.say(`У меня нету права \`Прикреплять файлы\``);
-  
+  if(!message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES')) return message.say(`У меня нету права \`Прикреплять файлы\``);
+  try {
   let URL = message.attachments.first() ? message.attachments.first().url : null || message.mentions.users.first() ? message.mentions.users.first().avatarURL({format: 'png', size: 2048}) : null|| message.author.avatarURL({format: 'png', size: 2048}) || message.id
-  const data = await loadImage(URL);
-  const canvas = createCanvas(data.width, data.height);
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(data, 0, 0);
-  grayscale(ctx, 0, 0, data.width, data.height);
-  const attachment = canvas.toBuffer();
-  if(Buffer.byteLength(attachment) > 8e+6) return message.say('Изображение было выше 8 МБ.');
-  return message.say({ files: [{ attachment, name: 'grayscale.png' }] });
+      const data = await loadImage(URL);
+      const canvas = createCanvas(data.width, data.height);
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(data, 0, 0);
+      grayscale(ctx, 0, 0, data.width, data.height);
+      const attachment = canvas.toBuffer();
+      if(Buffer.byteLength(attachment) > 8e+6) return message.say('Изображение было выше 8 МБ.');
+      return message.say({ files: [{ attachment, name: 'grayscale.png' }] });
+  } catch(err) {
+    message.say({ files: [{ attachment: "https://cdn.discordapp.com/attachments/623994687409225739/727564716788154438/nonegrayscale.png", name: 'grayscale.png' }] });
+  }
 };
-
 exports.help = {
   name: 'grayscale',
   aliases: [],
