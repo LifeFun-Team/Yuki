4f045995c36719149e67d079d3c96b79405ce1f2
Допи <57643140+XDopi@users.noreply.github.com>
2020-06-30 22:00:13 +0300

    Update np.js

--- commands/music/np.js
+++ commands/music/np.js
@@ -1,13 +1,14 @@
 const { MessageEmbed } = require("discord.js");
-
+const {invisible} = require('../../config.js');
+const moment = require("moment");
+require("moment-duration-format");
 exports.run = async (bot, message, args) => {
 const player = bot.music.queue.get(message.guild.id);
  if(!player) return message.channel.send("Ничего не играет");
   const song = player.songs[0];
 
-message.channel.send(`[__${song.info.title}__](${song.info.uri}) [${song.requestedBy}]\n${player.playing ? "▶ Играет" : "⏸ На паузе"} | Исполнитель: ${song.info.author}\n\n${player.songProgress(message)} [${player.duration(player.player.state.position)} / ${player.duration(song.info.length)}]`);
-
-}
+message.channel.send(new MessageEmbed().setDescription(`[__${song.info.title}__](${song.info.uri}) [${song.requestedBy}]\n${player.playing ? "▶ Играет" : "⏸ На паузе"} | Исполнитель: ${song.info.author}\n\n${player.songProgress(message)} [${moment.duration(player.player.state.position, "ms").format("hh:mm:ss", { trim: false })} / ${moment.duration(song.info.length, "ms").format("hh:mm:ss", { trim: false })}]`).setColor(invisible));
+};
 exports.help = {
   name: 'np',
   aliases: [],
