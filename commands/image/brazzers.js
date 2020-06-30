9f5158974ffe081cd9d598e86da64d2a1d523566
Допи <57643140+XDopi@users.noreply.github.com>
2020-06-30 20:24:19 +0300

    Update brazzers.js

--- commands/image/brazzers.js
+++ commands/image/brazzers.js
@@ -1,22 +1,24 @@
 const { createCanvas, loadImage } = require('canvas');
-
 exports.run = async (bot, message, args) => {
   if(!message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES')) return message.say("У меня нету права `Прикреплять файлы`");
+  try {
   let URL = message.attachments.first() ? message.attachments.first().url : null || message.mentions.users.first() ? message.mentions.users.first().avatarURL({format: 'png', size: 2048}) : null|| message.author.avatarURL({format: 'png', size: 2048}) || message.id
-  const base = await loadImage("https://media.discordapp.net/attachments/623994687409225739/715644258148679770/brazzers.png");
-  const avatar = await loadImage(URL);
-  const canvas = createCanvas(avatar.width, avatar.height);
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(avatar, 0, 0);
-  const ratio = base.width / base.height;
-  const width = avatar.width / 2;
-  const height = Math.round(width / ratio);
-  ctx.drawImage(base, 0, avatar.height - height, width, height);
-  const attachment = canvas.toBuffer();
-  if(Buffer.byteLength(attachment) > 8e+6) return message.say('Изображение было выше 8 МБ.');
-  return message.say({ files: [{ attachment, name: 'brazzers.png' }] });
-}
-
+      const base = await loadImage("https://media.discordapp.net/attachments/623994687409225739/715644258148679770/brazzers.png");
+      const avatar = await loadImage(URL);
+      const canvas = createCanvas(avatar.width, avatar.height);
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(avatar, 0, 0);
+      const ratio = base.width / base.height;
+      const width = avatar.width / 2;
+      const height = Math.round(width / ratio);
+      ctx.drawImage(base, 0, avatar.height - height, width, height);
+      const attachment = canvas.toBuffer();
+      if(Buffer.byteLength(attachment) > 8e+6) return message.say('Изображение было выше 8 МБ.');
+      return message.say({ files: [{ attachment, name: 'brazzers.png' }] });
+  } catch(err) {
+    message.say({ files: [{ attachment: "https://cdn.discordapp.com/attachments/623994687409225739/727557214461755443/nonebrazzers.png", name: 'brazzers.png' }] });
+  }
+};
 exports.help = {
   name: 'brazzers',
   aliases: [],
