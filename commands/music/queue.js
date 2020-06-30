d26980c75a8b4ed0ff3815e6cabcd93c6bd7c74f
Допи <57643140+XDopi@users.noreply.github.com>
2020-06-30 22:01:07 +0300

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,14 +1,14 @@
 const { MessageEmbed } = require("discord.js");
-
+const moment = require("moment");
+require("moment-duration-format");
 exports.run = async (bot, message, args) => {
 if(!message.member.voice.channel) return message.channel.send("Войдите в голосовой канал");
  const player = bot.music.queue.get(message.guild.id);
   if(!player) return message.channel.send("Ничего не играет");
    let i = 0;
 
-message.channel.send(`\`\`\`nimrod\nТекущий трек\n${player.songs[0].info.title} [${player.duration(player.player.state.position)} / ${player.duration(player.songs[0].info.length)}]\n────────────────────\n${player.songs[1] ? `${player.songs.map((songs) => `${i++}. ${songs.info.title} - ${player.duration(songs.info.length)}`).splice(1, 10).join("\n")}${player.songs.length > 11 ? `${player.songs.length - 11}...` : ""}` : "# Нет очереди"}\n\`\`\``)
-    
-}
+message.channel.send(`\`\`\`nimrod\nТекущий трек\n${player.songs[0].info.title} [${moment.duration(player.player.state.position, "ms").format("hh:mm:ss", { trim: false })} / ${moment.duration(player.songs[0].info.length, "ms").format("hh:mm:ss", { trim: false })}]\n────────────────────\n${player.songs[1] ? `${player.songs.map((songs) => `${i++}. ${songs.info.title} - ${moment.duration(songs.info.length, "ms").format("hh:mm:ss", { trim: false })}`).splice(1, 10).join("\n")}${player.songs.length > 11 ? `${player.songs.length - 11}...` : ""}` : "# Нет очереди"}\n\`\`\``)  
+};
 exports.help = {
   name: 'queue',
   aliases: ['q'],
