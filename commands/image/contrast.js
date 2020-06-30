be1741e1f54f3f7a7c447946b70a5d8a7105f03b
Допи <57643140+XDopi@users.noreply.github.com>
2020-06-30 20:27:06 +0300

    Update contrast.js

--- commands/image/contrast.js
+++ commands/image/contrast.js
@@ -1,8 +1,8 @@
-const { registerFont, createCanvas, loadImage } = require('canvas');
+const { createCanvas, loadImage } = require('canvas');
 const { contrast } = require('../../util/canvas');
 exports.run = async (bot, message, args) => {
   if(!message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES')) return message.say(`У меня нету права \`Прикреплять файлы\``);
-  
+  try {
   let URL = message.attachments.first() ? message.attachments.first().url : null || message.mentions.users.first() ? message.mentions.users.first().avatarURL({format: 'png', size: 2048}) : null|| message.author.avatarURL({format: 'png', size: 2048}) || message.id
 			const data = await loadImage(URL);
 			const canvas = createCanvas(data.width, data.height);
@@ -12,8 +12,10 @@ exports.run = async (bot, message, args) => {
 			const attachment = canvas.toBuffer();
 			if (Buffer.byteLength(attachment) > 8e+6) return message.say('Изображение было выше 8 МБ.');
 			return message.say({ files: [{ attachment, name: 'contrast.png' }] });
-	}
-
+	} catch(err) {
+    message.say({ files: [{ attachment: "https://cdn.discordapp.com/attachments/623994687409225739/727560574933925928/nonecontrast.png", name: 'contrast.png' }] });
+  }
+};
 exports.help = {
   name: 'contrast',
   aliases: [],
