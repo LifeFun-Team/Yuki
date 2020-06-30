59e490d9b382933c71f65c2bca5d91551f052579
Допи <57643140+XDopi@users.noreply.github.com>
2020-06-30 20:31:58 +0300

    Update sepia.js

--- commands/image/sepia.js
+++ commands/image/sepia.js
@@ -2,7 +2,7 @@ const { createCanvas, loadImage } = require('canvas');
 const { sepia } = require('../../util/canvas');
 exports.run = async (bot, message, args) => {
   if(!message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES')) return message.say(`У меня нету права \`Прикреплять файлы\``);
-  
+  try {
   let URL = message.attachments.first() ? message.attachments.first().url : null || message.mentions.users.first() ? message.mentions.users.first().avatarURL({format: 'png', size: 2048}) : null|| message.author.avatarURL({format: 'png', size: 2048}) || message.id
 			const data = await loadImage(URL);
 			const canvas = createCanvas(data.width, data.height);
@@ -12,8 +12,10 @@ exports.run = async (bot, message, args) => {
 			const attachment = canvas.toBuffer();
 			if (Buffer.byteLength(attachment) > 8e+6) return message.say('Изображение было выше 8 МБ.');
 			return message.say({ files: [{ attachment, name: 'sepia.png' }] });
-	}
-
+  } catch(err) {
+    message.say({ files: [{ attachment: "https://cdn.discordapp.com/attachments/623994687409225739/727566945242710036/nonesepia.png", name: 'sepia.png' }] });
+  }
+};
 exports.help = {
   name: 'sepia',
   aliases: [],
