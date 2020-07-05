3e1e4e41409dc845546e72d8213a349d2098b16b
Допи <57643140+XDopi@users.noreply.github.com>
2020-07-05 22:02:01 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require("discord.js");
-const shardMgr = new ShardingManager("./bot.js", { token: process.env.token, totalShards: 2, respawn: true });
+const shardMgr = new ShardingManager("./bot.js", { token: process.env.token, totalShards: "auto", respawn: true });
 
-shardMgr.spawn(2 === 2 ? shardMgr.totalShards : 2, 7500);
+shardMgr.spawn("auto" === "auto" ? shardMgr.totalShards : "auto", 7500);
 shardMgr.on("shardCreate", shard => console.log(`#${shard.id} осколок был запущен`));
