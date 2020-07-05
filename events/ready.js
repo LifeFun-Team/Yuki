4b0f2f9575b3910e52d9ad2c1af93fa982cadb79
Допи <57643140+XDopi@users.noreply.github.com>
2020-07-05 21:07:50 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,8 +2,6 @@ const SDC = require("@megavasiliy007/sdc-api");
 const sdc = new SDC("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxODE2MjExMDcxMDY3NzUyNCIsInBlcm1zIjowLCJpYXQiOjE1ODY2NzU0NDR9.ZBKs2yJlXhTlWD5SZMmar1gplx703AKxWBomhMp5oFQ");
 module.exports = (bot) => {
 console.log(`${bot.user.tag} Запустилась!`);
-const Music = require('../structures/Music.js');
-bot.music = new Music(bot);
 bot.user.setActivity("u!help", { type: "WATCHING" });
 sdc.setAutoPost(bot);
   
