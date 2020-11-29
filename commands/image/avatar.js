eece3368762078646dd90363d8db450147898c0c
Допи <57643140+XDopi@users.noreply.github.com>
2020-11-29 20:05:25 +0200

    Update avatar.js

--- commands/image/avatar.js
+++ commands/image/avatar.js
@@ -3,17 +3,8 @@ const { color } = require('../../config.js');
 exports.run = async (bot, message, args) => {
 
 let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member || message.id;
-  
-if(!message.channel.permissionsFor(message.guild.me).has("EMBED_LINKS")) return message.channel.send(`У меня нету права \`Встраивать ссылки \``);
-  
-let embed = new MessageEmbed()
-.setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-.setDescription("[${member.user.tag}]("+member.user.avatarURL({dynamic: true, size: 2048})+"): ")
-.setImage(member.user.avatarURL({dynamic: true, size: 2048}))
-.setColor(color)
-.setTimestamp();
+message.channel.send({ files: [{ attachment: member, name: `avatar.${member.avatar?.startWith('_a') ? 'gif' : 'jpeg'}`}] })
 
-message.channel.send(embed)
 };
 
 exports.help = {
