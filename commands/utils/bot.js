2424125218a9d11bb909bd3a55b11c21cbb4a558
Допи <57643140+XDopi@users.noreply.github.com>
2021-06-30 02:13:56 +0300

    Update bot.js

--- commands/utils/bot.js
+++ commands/utils/bot.js
@@ -8,7 +8,6 @@ exports.run = async (bot, message, args) => {
   const embed = new MessageEmbed()
   .addField(`Шардов`, `${bot.shard.count.toLocaleString()}`, true)
   .addField(`Серверов`, `${await bot.shard.fetchClientValues('guilds.cache.size').then(r => r.reduce((a,b) => Number(a+b).toLocaleString()))}`, true)
-  .addField(`Пользователей`, `${await bot.shard.fetchClientValues('users.cache.size').then(r => r.reduce((a,b) => Number(a+b).toLocaleString()))}`, true)
   .addField(`Команд`, `${bot.commands.size.toLocaleString()}`, true)
   .addField(`Время работы`,  moment.duration(bot.uptime).format(` D [День], H [час(ов)], m [минут(а)], s [Секунд(а)]`))
   .addField(`Discord.js`, `v${version}`, true)
